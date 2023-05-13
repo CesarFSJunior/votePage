6b733bc1bae2a6a39b890e6e506078cc1c323fb3
Cesar Francisco <cesarfsjunior@hotmail.com>
2023-05-13 20:41:10 -0300

    project created

---
+++ src/main/java/com/cesarFrancisco/votePage/VotePageApplication.java
@@ -0,0 +1,13 @@
+package com.cesarFrancisco.votePage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotePageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotePageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cesarFrancisco/votePage/VotePageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cesarFrancisco.votePage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotePageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
